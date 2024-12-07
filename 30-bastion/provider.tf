1ff0b80c336819c112112c55fb9b5c7af92643f9
Sai-kamineni <saikumar.dba10@gmail.com>
2024-12-07 12:29:39 +0530

    tfinfradev

--- 30-bastion/provider.tf
+++ 30-bastion/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
+    bucket = "81s-multi-env-dev"
     key    = "expense-bastion"
     region = "us-east-1"
-    dynamodb_table = "81s-locking"
+    dynamodb_table = "81s-locking-dev"
   }
 }
 
